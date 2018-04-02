9388539bb6e9c68b9ed5b37f822e8f13c44d4f99
xuzhuoxi <xuzhuoxi@gmail.com>
2018-04-02 18:01:39 +0800

    1.更新依赖库codeutil_1.0.jar，修改序列化二进制时的错误。 2.更新单元测试。

--- test/cfg/ConfigReaderTest.java
+++ test/cfg/ConfigReaderTest.java
@@ -1,29 +1,18 @@
 package cfg;
 
-import java.io.DataInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.Test;
 
 import cfg.serialize.FieldKey;
 import cfg.serialize.FieldRangeType;
-import cfg.serialize.cfgdata.BinarySheetHandler;
 import cfg.serialize.cfgdata.JsonSheetHandler;
 import cfg.settings.Settings;
 import cfg.source.WorkbookInfo;
 import cfg.source.data.SheetInfo;
-import code.file.FileUtils;
 import code.lang.NumberUtil;
 import code.path.BasePathUtils;
-import xu.BinaryReaderProxy;
-import xu.CfgBuilding;
 
 public class ConfigReaderTest {
 
@@ -55,71 +44,6 @@ public class ConfigReaderTest {
 			handler.config(sheetInfo);
 			Object out = handler.serialize(FieldRangeType.Client, FieldKey.Json);
 			System.out.println(out);
-		}
-	}
-
-	@Test
-	public void testJson2Objects() {
-		String basePath = AppDefine.instance.getBasePath();
-		String filePath = basePath + "/target/data/client/cfg_building_c.json";
-		System.out.println("FilePaht:" + filePath);
-		String jsonContent = FileUtils.readFileContent(filePath);
-		System.out.println("JsonContent:" + jsonContent);
-		JSONArray ary = new JSONArray(jsonContent);
-		int len = ary.length();
-		System.out.println("Length:" + len);
-		for (int i = 0; i < len; i++) {
-			JSONObject jObj = ary.getJSONObject(i);
-			CfgBuilding obj = new CfgBuilding();
-			obj.parseJson(jObj);
-			System.out.println(obj);
-		}
-	}
-
-	@Test
-	public void testWorkbook2Binary() {
-		String basePath = AppDefine.instance.getBasePath();
-		String filePath = basePath + "/source/cfg_building.xls";
-		System.out.println("FilePaht:" + filePath);
-		WorkbookInfo info = new WorkbookInfo(filePath);
-		info.loadSheetInfos();
-
-		BinarySheetHandler handler = new BinarySheetHandler();
-		List<SheetInfo> sheets = info.getSheetInfos();
-		for (SheetInfo sheetInfo : sheets) {
-			handler.config(sheetInfo);
-			byte[] out = (byte[]) handler.serialize(FieldRangeType.Client, FieldKey.Json);
-			System.out.println(sheetInfo);
-			System.out.println(out.length);
-		}
-	}
-
-	@Test
-	public void testBinary2Objects() {
-		String basePath = AppDefine.instance.getBasePath();
-		String filePath = basePath + "/target/data/client/cfg_building_c.binary";
-		System.out.println("FilePaht:" + filePath);
-
-		InputStream is;
-		DataInputStream dis;
-		try {
-			is = new FileInputStream(filePath);
-			dis = new DataInputStream(is);
-			BinaryReaderProxy proxy = new BinaryReaderProxy(dis, "utf-8");
-			while (true) {
-				CfgBuilding obj = new CfgBuilding();
-				obj.parseBinary(proxy);
-				System.out.println(obj);
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
 
--- test/cfg/serialize/SerializeTest.java
+++ test/cfg/serialize/SerializeTest.java
@@ -7,15 +7,33 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import cfg.AppDefine;
+import cfg.serialize.cfgdata.BinarySheetHandler;
 import cfg.source.WorkbookInfo;
 import cfg.source.data.SheetInfo;
+import code.file.FileUtils;
+import code.lang.NumberUtil;
 import xu.BinaryReaderProxy;
 import xu.CfgBuilding;
 
 public class SerializeTest {
+
+	@Test
+	public void testNumber2Binary() {
+		int i = 99;
+		byte[] ba = NumberUtil.toByteArray(i, 2);
+		int i2 = NumberUtil.toShort(ba);
+		System.out.println(i2);
+		String str = "";
+		for (byte b : ba) {
+			str = str + " " + b;
+		}
+		System.out.println(str);
+	}
 
 	@Test
 	public void testDataFile() {
@@ -33,6 +51,24 @@ public class SerializeTest {
 					basePath + "/../testres/dist/data/client", FieldKey.Json);
 			SerializeDataUtil.serializeData(sheetInfo, OutputDataFormat.Binary, FieldRangeType.Client,
 					basePath + "/../testres/dist/data/client", null);
+		}
+	}
+
+	@Test
+	public void testWorkbook2Binary() {
+		String basePath = AppDefine.instance.getBasePath();
+		String filePath = basePath + "/source/cfg_building.xls";
+		System.out.println("FilePaht:" + filePath);
+		WorkbookInfo info = new WorkbookInfo(filePath);
+		info.loadSheetInfos();
+
+		BinarySheetHandler handler = new BinarySheetHandler();
+		List<SheetInfo> sheets = info.getSheetInfos();
+		for (SheetInfo sheetInfo : sheets) {
+			handler.config(sheetInfo);
+			byte[] out = (byte[]) handler.serialize(FieldRangeType.Client, FieldKey.Json);
+			System.out.println(sheetInfo);
+			System.out.println(out.length);
 		}
 	}
 
@@ -66,9 +102,9 @@ public class SerializeTest {
 	}
 
 	@Test
-	public void testBinaryReader() {
+	public void testBinary2Objects() {
 		String basePath = AppDefine.instance.getBasePath();
-		String filePath = basePath + "/dist/data/server/cfg_building.sj";
+		String filePath = basePath + "/../testres/dist/data/client/cfg_building_c.binary";
 		File file = new File(filePath);
 		DataInputStream dis = null;
 		try {
@@ -85,6 +121,24 @@ public class SerializeTest {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testJson2Objects() {
+		String basePath = AppDefine.instance.getBasePath();
+		String filePath = basePath + "/target/data/client/cfg_building_c.json";
+		System.out.println("FilePath:" + filePath);
+		String jsonContent = FileUtils.readFileContent(filePath);
+		System.out.println("JsonContent:" + jsonContent);
+		JSONArray ary = new JSONArray(jsonContent);
+		int len = ary.length();
+		System.out.println("Length:" + len);
+		for (int i = 0; i < len; i++) {
+			JSONObject jObj = ary.getJSONObject(i);
+			CfgBuilding obj = new CfgBuilding();
+			obj.parseJson(jObj);
+			System.out.println(obj);
 		}
 	}
 

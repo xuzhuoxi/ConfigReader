5d4a531b6d5149edaed889e450f3e0e3291fbc99
xuzhuoxi <xuzhuoxi@gmail.com>
2021-01-24 17:45:27 +0800

    1.增加功能：Excel中对应类名、导出文件名如果为空，相应文件导出时将被忽略(以前为抛出异常并结束，现在为抛出异常后继续)。

--- src/cfg/serialize/SerializeDataUtil.java
+++ src/cfg/serialize/SerializeDataUtil.java
@@ -13,78 +13,66 @@ import code.file.FileUtils;
 import code.lang.StringUtil;
 
 public class SerializeDataUtil {
-	/**
-	 * 导出数据<br>
-	 * 如果fieldRange对应的字段数为0，则直接跳过
-	 * 
-	 * @param sheetInfo
-	 *            Excel中的一个sheet数据对象
-	 * @param fileFormat
-	 *            输出文件格式
-	 * @param fieldRange
-	 *            导出的字段范围类型
-	 * @param outputFolder
-	 *            文件输出目录
-	 * @param fieldKey
-	 *            字段使用的键值类型
-	 * @throws SheetDataException
-	 *             数据异常信息
-	 */
-	public static void serializeData(SheetInfo sheetInfo, OutputDataFormat fileFormat, FieldRangeType fieldRange,
-			String outputFolder, FieldKey fieldKey) throws SheetDataException {
-		if (sheetInfo.getDefine().isFieldRangeEmpty(fieldRange)) {// 跳过无索引字段的表
-			return;
-		}
-		ISheetHandler handler = SheetHandlerFactory.getSheetHandler(fileFormat);
-		handler.config(sheetInfo);
-		Object out = handler.serialize(fieldRange, fieldKey);
-		String fileName = sheetInfo.getDefine().getExportInfo(fieldRange).getFileName();
-		if (fileName.length() == 0) {
-			Exception e = new SheetDefineException("FileName is Empty: " + sheetInfo.getSheetNamed());
-			e.printStackTrace();
-			System.exit(1);
-		}
-		String outputFilePath = outputFolder + "/" + fileName + "." + fileFormat.getValue();
-		if (fileFormat.isTextFile()) {
-			System.out.println("\t输出数据文件(字符)(" + Settings.getInstance().getSysSettings().getTargetEncoding() + ")："
-					+ outputFilePath);
-			Charset outputCharset = Settings.getInstance().getSysSettings().getTargetCharset();
-			String outJsonStr = (String) out;
-			if (!Settings.getInstance().getSysSettings().isEncodingConsistent()) {
-				outJsonStr = StringUtil.changeCharset(outJsonStr,
-						Settings.getInstance().getSysSettings().getSourceCharset(), outputCharset);
-			}
-			FileUtils.writeTextFile(outputFilePath, outJsonStr, outputCharset);
-			// FileUtils.writeTextFile(outputFilePath, (String) out,
-			// outputCharset);
-			// System.out.println(out);
-		} else {
-			System.out.println("\t输出数据文件(字节):" + outputFilePath);
-			FileUtils.writeBinaryFile(outputFilePath, (byte[]) out);
-		}
-	}
+    /**
+     * 导出数据<br>
+     * 如果fieldRange对应的字段数为0，则直接跳过
+     *
+     * @param sheetInfo    Excel中的一个sheet数据对象
+     * @param fileFormat   输出文件格式
+     * @param fieldRange   导出的字段范围类型
+     * @param outputFolder 文件输出目录
+     * @param fieldKey     字段使用的键值类型
+     * @throws SheetDataException 数据异常信息
+     */
+    public static void serializeData(SheetInfo sheetInfo, OutputDataFormat fileFormat, FieldRangeType fieldRange,
+                                     String outputFolder, FieldKey fieldKey) throws SheetDataException {
+        if (sheetInfo.getDefine().isFieldRangeEmpty(fieldRange)) {// 跳过无索引字段的表
+            return;
+        }
+        ISheetHandler handler = SheetHandlerFactory.getSheetHandler(fileFormat);
+        handler.config(sheetInfo);
+        Object out = handler.serialize(fieldRange, fieldKey);
+        String fileName = sheetInfo.getDefine().getExportInfo(fieldRange).getFileName();
+        if (null == fileName || fileName.length() == 0) {
+            Exception e = new SheetDefineException("FileName is Empty: " + sheetInfo.getSheetNamed());
+            e.printStackTrace();
+            return;
+        }
+        String outputFilePath = outputFolder + "/" + fileName + "." + fileFormat.getValue();
+        if (fileFormat.isTextFile()) {
+            System.out.println("\t输出数据文件(字符)(" + Settings.getInstance().getSysSettings().getTargetEncoding() + ")："
+                    + outputFilePath);
+            Charset outputCharset = Settings.getInstance().getSysSettings().getTargetCharset();
+            String outJsonStr = (String) out;
+            if (!Settings.getInstance().getSysSettings().isEncodingConsistent()) {
+                outJsonStr = StringUtil.changeCharset(outJsonStr,
+                        Settings.getInstance().getSysSettings().getSourceCharset(), outputCharset);
+            }
+            FileUtils.writeTextFile(outputFilePath, outJsonStr, outputCharset);
+            // FileUtils.writeTextFile(outputFilePath, (String) out,
+            // outputCharset);
+            // System.out.println(out);
+        } else {
+            System.out.println("\t输出数据文件(字节):" + outputFilePath);
+            FileUtils.writeBinaryFile(outputFilePath, (byte[]) out);
+        }
+    }
 
-	/**
-	 * 批量导出数据
-	 * 
-	 * @param sheetInfos
-	 *            Excel中的一个sheet数据对象组成的数组
-	 * @param fileFormat
-	 *            输出文件格式
-	 * @param fieldRange
-	 *            导出的字段范围类型
-	 * @param outputFolder
-	 *            文件输出目录
-	 * @param fieldKey
-	 *            字段使用的键值类型
-	 * @throws SheetDataException
-	 *             数据异常信息
-	 */
-	public static void serializeData(List<SheetInfo> sheetInfos, OutputDataFormat fileFormat, FieldRangeType fieldRange,
-			String outputFolder, FieldKey fieldKey) throws SheetDataException {
-		for (SheetInfo sheetInfo : sheetInfos) {
-			System.out.println("Start Serialize Data: [" + sheetInfo.getSheetNamed() + "," + fileFormat + "]");
-			serializeData(sheetInfo, fileFormat, fieldRange, outputFolder, fieldKey);
-		}
-	}
+    /**
+     * 批量导出数据
+     *
+     * @param sheetInfos   Excel中的一个sheet数据对象组成的数组
+     * @param fileFormat   输出文件格式
+     * @param fieldRange   导出的字段范围类型
+     * @param outputFolder 文件输出目录
+     * @param fieldKey     字段使用的键值类型
+     * @throws SheetDataException 数据异常信息
+     */
+    public static void serializeData(List<SheetInfo> sheetInfos, OutputDataFormat fileFormat, FieldRangeType fieldRange,
+                                     String outputFolder, FieldKey fieldKey) throws SheetDataException {
+        for (SheetInfo sheetInfo : sheetInfos) {
+            System.out.println("Start Serialize Data: [" + sheetInfo.getSheetNamed() + "," + fileFormat + "]");
+            serializeData(sheetInfo, fileFormat, fieldRange, outputFolder, fieldKey);
+        }
+    }
 }
--- src/cfg/serialize/SerializeDefineUtil.java
+++ src/cfg/serialize/SerializeDefineUtil.java
@@ -10,60 +10,50 @@ import cfg.source.data.SheetInfo;
 import code.file.FileUtils;
 
 public class SerializeDefineUtil {
-	/**
-	 * 导出定义数据结构
-	 * 
-	 * @param sheetInfo
-	 *            Excel中的一个sheet数据对象
-	 * @param fieldRange
-	 *            导出的字段范围类型
-	 * @param lang
-	 *            编码语言
-	 * @param outputFolder
-	 *            输出文件夹
-	 * @param extNamed
-	 *            输出文件的扩展名
-	 */
-	public static void serializeDefine(SheetInfo sheetInfo, FieldRangeType fieldRange, OutputDefineLangType lang,
-			String outputFolder, String extNamed) {
-		if (sheetInfo.getDefine().isFieldRangeEmpty(fieldRange)) {// 跳过无索引字段的表
-			return;
-		}
-		IDefineGenerater dg = new DefineGenerater();
-		dg.setInfo(lang, fieldRange);
-		String value = dg.serialize(sheetInfo.getDefine());
+    /**
+     * 导出定义数据结构
+     *
+     * @param sheetInfo    Excel中的一个sheet数据对象
+     * @param fieldRange   导出的字段范围类型
+     * @param lang         编码语言
+     * @param outputFolder 输出文件夹
+     * @param extNamed     输出文件的扩展名
+     */
+    public static void serializeDefine(SheetInfo sheetInfo, FieldRangeType fieldRange, OutputDefineLangType lang,
+                                       String outputFolder, String extNamed) {
+        if (sheetInfo.getDefine().isFieldRangeEmpty(fieldRange)) {// 跳过无索引字段的表
+            return;
+        }
+        IDefineGenerater dg = new DefineGenerater();
+        dg.setInfo(lang, fieldRange);
+        String value = dg.serialize(sheetInfo.getDefine());
 
-		String className = sheetInfo.getDefine().getExportInfo(fieldRange).getClassName();
-		if (className.length() == 0) {
-			Exception e = new SheetDefineException("ClassName is Empty: " + sheetInfo.getSheetNamed());
-			e.printStackTrace();
-			System.exit(1);
-		}
-		String outputFilePath = outputFolder + "/" + className + "." + extNamed;
-		System.out.println(
-				"\t输出定义文件(" + Settings.getInstance().getSysSettings().getTargetEncoding() + ")：" + outputFilePath);
-		FileUtils.writeTextFile(outputFilePath, value, Settings.getInstance().getSysSettings().getTargetCharset());
-	}
+        String className = sheetInfo.getDefine().getExportInfo(fieldRange).getClassName();
+        if (null == className || className.length() == 0) {
+            Exception e = new SheetDefineException("ClassName is Empty: " + sheetInfo.getSheetNamed());
+            e.printStackTrace();
+            return;
+        }
+        String outputFilePath = outputFolder + "/" + className + "." + extNamed;
+        System.out.println(
+                "\t输出定义文件(" + Settings.getInstance().getSysSettings().getTargetEncoding() + ")：" + outputFilePath);
+        FileUtils.writeTextFile(outputFilePath, value, Settings.getInstance().getSysSettings().getTargetCharset());
+    }
 
-	/**
-	 * 导出定义数据结构
-	 * 
-	 * @param sheetInfos
-	 *            Excel中的一个sheet数据对象组成的数组
-	 * @param fieldRange
-	 *            导出的字段范围类型
-	 * @param lang
-	 *            编码语言
-	 * @param outputFolder
-	 *            输出文件夹
-	 * @param extNamed
-	 *            输出文件的扩展名
-	 */
-	public static void serializeDefine(List<SheetInfo> sheetInfos, FieldRangeType fieldRange, OutputDefineLangType lang,
-			String outputFolder, String extNamed) {
-		for (SheetInfo sheetInfo : sheetInfos) {
-			System.out.println("Start Serialize Define: [" + sheetInfo.getSheetNamed() + "," + lang + "]");
-			serializeDefine(sheetInfo, fieldRange, lang, outputFolder, extNamed);
-		}
-	}
+    /**
+     * 导出定义数据结构
+     *
+     * @param sheetInfos   Excel中的一个sheet数据对象组成的数组
+     * @param fieldRange   导出的字段范围类型
+     * @param lang         编码语言
+     * @param outputFolder 输出文件夹
+     * @param extNamed     输出文件的扩展名
+     */
+    public static void serializeDefine(List<SheetInfo> sheetInfos, FieldRangeType fieldRange, OutputDefineLangType lang,
+                                       String outputFolder, String extNamed) {
+        for (SheetInfo sheetInfo : sheetInfos) {
+            System.out.println("Start Serialize Define: [" + sheetInfo.getSheetNamed() + "," + lang + "]");
+            serializeDefine(sheetInfo, fieldRange, lang, outputFolder, extNamed);
+        }
+    }
 }
